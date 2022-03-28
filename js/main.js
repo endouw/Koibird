85cf1c265fe93c72b575b9cfb85e55cbecd9c498
Endrit Ajrulla <ajrulaendrit@gmail.com>
2022-03-28 12:22:04 +0200

    Initial

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const mobile = document.querySelector(".mobile");
+const menu = document.querySelector(".menu");
+const favorite = document.querySelectorAll(".favorite");
+const dropdown = document.querySelector(".dropdown");
+const dropdownMenu = document.querySelector(".dropdown__menu");
+let dropdownButton = document.querySelector('.dropdown__button');
+
+
+// Mobile toggle
+mobile.addEventListener("click", () => {
+  menu.classList.toggle("displayBlock");
+});
+
+// Wishlist toggle
+function addToWishlist(e) {
+  e.target.classList.toggle("active");
+}
+
+favorite.forEach((wish) => {
+  wish.addEventListener("click", addToWishlist);
+});
+
+// Dropdown button
+let dropdownButtonState = true;
+
+// Toggle inner html of dropdownMenu span
+function toggleDropdown() {
+  if (dropdownButtonState) {
+    dropdownButton.innerHTML = `<svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 1L7 7L13 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `;
+    dropdownButtonState = false;
+  } else {
+    dropdownButton.innerHTML = `<svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13 7L7 1L1 7" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `;
+    dropdownButtonState = true;
+  }
+}
+
+dropdownButton.addEventListener("click", toggleDropdown);
+
+// responsive dropdown menu
+function toggleDropdownMenu() {
+  dropdownMenu.classList.toggle("displayGrid");
+}
+
+dropdownButton.addEventListener("click", toggleDropdownMenu);
